fbd87a22fe49feeed9204c402e4d96ff96c2044a
Radosław Pudełkiewicz <radoslaw.pudelkiewicz@gmail.com>
2022-11-29 21:32:23 +0100

    Add parser for integer array literals

---
+++ WebRepeatedNumbersSieve.Tests/Models/ArrayParsers/IntegerArrayParserUnitTests.cs
@@ -0,0 +1,36 @@
+﻿using WebRepeatedNumbersSieve.Models.ArrayParsers;
+
+namespace WebRepeatedNumbersSieve.Tests.Models.ArrayParsers
+{
+    public class IntegerArrayParserUnitTests
+    {
+        private readonly IntegerArrayParser _parser = new IntegerArrayParser();
+
+        [Test]
+        public void ShouldThrowArgumentExceptionForIncorrectArrayLiteral()
+        {
+            Assert.Throws<ArgumentNullException>(() => _parser.Parse(null));
+        }
+
+
+        [TestCase("")]
+        [TestCase("   ")]
+        [TestCase("1,2,3,4,5,1,1")]
+        [TestCase("[1,2,3,4,5,1,1")]
+        [TestCase("1,2,3,4,5,1,1]")]
+        [TestCase("[112, ,2,1,9,9,8]")]
+        [TestCase("[112, he3,2,1,9,9,8]")]
+        public void ShouldThrowArgumentExceptionForIncorrectArrayLiteral(string inputArrayLiteral)
+        {
+            Assert.Throws<ArgumentException>(() => _parser.Parse(inputArrayLiteral));
+        }
+
+        [TestCase("[]", new int[] { })]
+        [TestCase("[4]", new int[] { 4 })]
+        [TestCase("[1,2,3,4,5,1,1]", new int[] { 1, 2, 3, 4, 5, 1, 1 })]
+        public void ShouldParseArrayLiteral(string inputArrayLiteral, int[] expectedOutputArray)
+        {
+            Assert.IsTrue(Enumerable.SequenceEqual(expectedOutputArray, _parser.Parse(inputArrayLiteral)));
+        }
+    }
+}
---
+++ WebRepeatedNumbersSieve/Models/ArrayParsers/IntegerArrayParser.cs
@@ -0,0 +1,52 @@
+﻿namespace WebRepeatedNumbersSieve.Models.ArrayParsers
+{
+    public class IntegerArrayParser : IArrayParser<int>
+    {
+        public int[] Parse(string arrayLiteral)
+        {
+            if (arrayLiteral == null)
+            {
+                throw new ArgumentNullException("No array literal specified!");
+            }
+
+            arrayLiteral = arrayLiteral.Trim();
+
+            if (!arrayLiteral.StartsWith('['))
+            {
+                throw new ArgumentException("Array literal should start with '[' character!");
+            }
+
+            if (!arrayLiteral.EndsWith(']'))
+            {
+                throw new ArgumentException("Array literal should end with ']' character!");
+            }
+
+            var elementLiterals = arrayLiteral.Substring(1, arrayLiteral.Length - 2).Split(',');
+
+            if (elementLiterals.Length == 1 && String.IsNullOrWhiteSpace(elementLiterals[0]))
+            {
+                return Array.Empty<int>();
+            }
+
+            var intArray = new int[elementLiterals.Length];            
+            int element;
+
+            for (int i = 0; i < elementLiterals.Length; i++)
+            {
+                if (String.IsNullOrWhiteSpace(elementLiterals[i]))
+                {
+                    throw new ArgumentException($"Element at index: {i} of the array should be specified!");
+                }
+
+                if (!int.TryParse(elementLiterals[i].Trim(), out element))
+                {
+                    throw new ArgumentException($"Can not parse to integer an element '{elementLiterals[i].Trim()}' at index: {i} of the array!");
+                }
+
+                intArray[i] = element;
+            }
+
+            return intArray;
+        }
+    }
+}
