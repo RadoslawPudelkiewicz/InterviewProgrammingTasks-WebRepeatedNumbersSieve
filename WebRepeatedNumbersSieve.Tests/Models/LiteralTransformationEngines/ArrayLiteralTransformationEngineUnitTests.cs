1e1f0cdcfb8e166f2f42d07274d8ca1c928d1c2f
Radosław Pudełkiewicz <radoslaw.pudelkiewicz@gmail.com>
2022-11-29 22:45:18 +0100

    Add array literal transformation engine class

---
+++ WebRepeatedNumbersSieve.Tests/Models/LiteralTransformationEngines/ArrayLiteralTransformationEngineUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using WebRepeatedNumbersSieve.Models;
+using WebRepeatedNumbersSieve.Models.LiteralTransformationEngines;
+
+namespace WebRepeatedNumbersSieve.Tests.Models.LiteralTransformationEngines
+{
+    public class ArrayLiteralTransformationEngineUnitTests
+    {
+        [Test]
+        public void ShouldTransformArrayLiteral()
+        {
+            var arrayLiteral = "[1,2]";
+
+            var parserMock = new Mock<IArrayParser<int>>();
+            parserMock.Setup(p => p.Parse(arrayLiteral))
+                .Returns(new int[] { 1, 2 });
+
+            var serializerMock = new Mock<IArraySerializer<int>>();
+            serializerMock.Setup(s => s.Serialize(new int[] { 1, 2, 3, 4 }))
+                .Returns("[1,2,3,4]");
+
+            var firstTransformation = new Mock<IArrayTransformation<int>>();
+            firstTransformation.Setup(t => t.Transform(new int[] { 1, 2 }))
+                .Returns(new int[] { 1, 2, 3 });
+
+            var secoundTransformation = new Mock<IArrayTransformation<int>>();
+            secoundTransformation.Setup(t => t.Transform(new int[] { 1, 2, 3 }))
+                .Returns(new int[] { 1, 2, 3, 4 });
+
+            var engine = new ArrayLiteralTransformationEngine<int>(parserMock.Object, serializerMock.Object,
+                new IArrayTransformation<int>[] { firstTransformation.Object, secoundTransformation.Object });
+
+            Assert.That(engine.DoLiteralTransformation(arrayLiteral), Is.EqualTo("[1,2,3,4]"));
+        }
+    }
+}
---
+++ WebRepeatedNumbersSieve/Models/LiteralTransformationEngines/ArrayLiteralTransformationEngine.cs
@@ -0,0 +1,40 @@
+﻿namespace WebRepeatedNumbersSieve.Models.LiteralTransformationEngines
+{
+    public class ArrayLiteralTransformationEngine<T> : ILiteralTransformationEngine
+    {
+        private readonly IArrayParser<T> _parser;
+        private readonly IArraySerializer<T> _serializer;
+        private readonly IEnumerable<IArrayTransformation<T>> _arrayTransformations;
+        
+
+        public ArrayLiteralTransformationEngine(
+            IArrayParser<T> parser,
+            IArraySerializer<T> serializer,
+            IEnumerable<IArrayTransformation<T>> arrayTransformations)
+        {
+            _parser = parser;
+            _serializer = serializer;
+            _arrayTransformations = arrayTransformations;
+        }
+
+        public string DoLiteralTransformation(string literal)
+        {
+            try
+            {
+                //var arr = IntegerArray.Parse(literal);
+                var arr = _parser.Parse(literal);
+
+                foreach (var transformations in _arrayTransformations)
+                {
+                    arr = transformations.Transform(arr);
+                }
+
+                return _serializer.Serialize(arr);
+            }
+            catch (Exception ex) when (ex is ArgumentNullException || ex is ArgumentException)
+            {
+                return ex.Message;
+            }
+        }
+    }
+}
