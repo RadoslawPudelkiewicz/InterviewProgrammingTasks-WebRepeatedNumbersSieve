fb7a0198c419c02946d8e397a581f3dc196a5ea0
Radosław Pudełkiewicz <radoslaw.pudelkiewicz@gmail.com>
2022-11-29 20:42:48 +0100

    Add descending sorter array transformation

---
+++ WebRepeatedNumbersSieve.Tests/Models/ArrayTransformations/DescendingArraySorterUnitTests.cs
@@ -0,0 +1,17 @@
+﻿using WebRepeatedNumbersSieve.Models.ArrayTransformations;
+
+namespace WebRepeatedNumbersSieve.Tests.Models.ArrayTransformations
+{
+    public class DescendingArraySorterUnitTests
+    {
+        [TestCase(new int[] { }, new int[] { })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, }, new int[] { 5, 4, 3, 2, 1 })]
+        [TestCase(new int[] { 1, 2, 1, 3, 4, 3, 5, }, new int[] { 5, 4, 3, 3, 2, 1, 1 })]
+        public void ShouldSortInDescendingOrder(int[] inputArray, int[] expectedOutputArray)
+        {
+            var sorter = new DescendingArraySorter<int>();
+
+            Assert.IsTrue(Enumerable.SequenceEqual(expectedOutputArray, sorter.Transform(inputArray)));
+        }
+    }
+}
---
+++ WebRepeatedNumbersSieve/Models/ArrayTransformations/DescendingArraySorter.cs
@@ -0,0 +1,10 @@
+﻿namespace WebRepeatedNumbersSieve.Models.ArrayTransformations
+{
+    public class DescendingArraySorter<T> : IArrayTransformation<T> where T : IComparable<T>
+    {
+        public T[] Transform(T[] arrayToTransform)
+        {
+            return arrayToTransform.OrderByDescending(x => x).ToArray();
+        }
+    }
+}
